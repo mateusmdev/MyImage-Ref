4a0db80566f0377fc1df44a0ac88b43da3f5e657
Mateus Melo <mateus.mdeveloper@gmail.com>
2026-09-12 22:34:42 -0300

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+</style>
